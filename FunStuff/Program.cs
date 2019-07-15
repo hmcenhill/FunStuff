a6b68f8749558895d16f549a429913b20c843e1c
Hermann <hmcenhill@gmail.com>
2019-07-14 21:58:49 -0400

    Initial VS file structure

---
+++ FunStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
